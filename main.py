08e8962b618e7f15bedce90745986aa6ff6f3a28
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-10-12 17:26:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#ejercicio 1
+n= [1,2,3]
+print(n)
+print("")
+#punto1
+print(n[:3])
+print("")
+n= [1,2,3]
+n.sort()
+print(n)
+print("")
+#punto 2
+n= [1,2,3]
+n.sort(reverse= True)
+print(n)
+print("")
+#punto 3
+p=  [0,2,4,6]
+print(p)
+print("")
+p.sort()
+print(p)
+print("")
+p.sort(reverse=True)
+print(p)
+print("")
+#ejercicio 2
+a= (1,2,3)
+b= (4,5,6)
+r= (a,b)
+print(r)
+print("")
+#punto 1 
+print(r[:5])
+print("")
+#punto 2 
+print(b) 
+print(a)
+print("")
+a= [1,2,3]
+b= [4,5,6]
+r= [a,b]
+r.sort(reverse=True)
+print(r)
+print("")
+#punto 3
+c= (0,1,2,3)
+print(c)
+print(c[:4])
+print(c[0])
+print("")
+#ejercicio 3
