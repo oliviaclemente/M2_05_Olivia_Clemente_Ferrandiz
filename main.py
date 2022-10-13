b0104d4e86defc14fd9eb0c058efbfd8d071c242
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-10-13 10:55:39 +0000

    commit

--- main.py
+++ main.py
@@ -1,52 +1,30 @@
 #ejercicio 1
-n= [1,2,3]
-print(n)
-print("")
-#punto1
-print(n[:3])
-print("")
-n= [1,2,3]
-n.sort()
-print(n)
-print("")
-#punto 2
-n= [1,2,3]
-n.sort(reverse= True)
-print(n)
-print("")
-#punto 3
-p=  [0,2,4,6]
-print(p)
-print("")
-p.sort()
-print(p)
-print("")
-p.sort(reverse=True)
-print(p)
+a = int(input("Introduce un número:"))
+b = int(input("Introduce un número diferente:"))
+c = int(input("Introduce otro número"))
+if a == 0:
+  print("No es correcto ya que es 0")
+elif a < b < c:
+  print("Se muestra en orden ascendente")
+else:
+  print("Se muestra en orden descendente")
 print("")
 #ejercicio 2
-a= (1,2,3)
-b= (4,5,6)
-r= (a,b)
-print(r)
-print("")
-#punto 1 
-print(r[:5])
-print("")
-#punto 2 
-print(b) 
-print(a)
-print("")
-a= [1,2,3]
-b= [4,5,6]
-r= [a,b]
-r.sort(reverse=True)
-print(r)
-print("")
-#punto 3
-c= (0,1,2,3)
-print(c)
-print(c[:4])
-print(c[0])
+valores = []
+print("Introduce 3 valores")
+for x in range(3):
+  valores.append(input("Intrduce un valor:"))
+print(valores)
+if valores == 0:
+  print("No es correcto ya que es 0")
+elif a<b<c:
+  print("Se muestra en orden ascendente")
+else:
+  print("Se muestra en orden descendente")
 print("")
 #ejercicio 3
+letra = []
+print("Introduce 3 letras:")
+for x in range(3):
+  letra.append(input("Introduce una letra:"))
+print(letra)
